321f4eab1e8a26b45acad750c96f96bd565714c0
Francisco Murillo <fmurillom@gmail.com>
2020-07-23 16:04:21 -0600

    [ADD] Fixes al calendarizador y añadido inicio algoritmo para puente Y

--- aliensycalendarizadorTEMP/main.c
+++ aliensycalendarizadorTEMP/main.c
@@ -40,6 +40,27 @@ struct argsCalenPrior
     int cantExtremos;
 };
 
+struct argsBridgeY
+{
+    llist *extremoN;
+    llist *extremoS;
+    pthread_mutex_t *lockExtremoN;
+    pthread_mutex_t *lockExtremoS;
+    int Y;
+    int lenBr;
+    int pesoSoport;
+    int generatingN;
+    int generatingS;
+};
+
+struct alienCruzando{
+    pthread_t tid;
+    struct Alien *alien;
+    int pos;
+    int indice;
+
+};
+
 void numprint(void *);
 
 double ran_expo(double lambda){
@@ -48,14 +69,7 @@ double ran_expo(double lambda){
     return -log(1- u) / lambda;
 }
 
-void *alienGen(void *arguments){/*
-    llist *mylist = ((struct argsGen *)arguments)->mylist;
-    double interval = ((struct argsGen *)arguments)->interval;
-    int total = ((struct argsGen *)arguments)->total;
-    int velocidad = ((struct argsGen *)arguments)->velocidad;
-    pthread_mutex_t *lockColonia = ((struct argsGen *)arguments)->lockColonia;
-    int *generating = &((struct argsGen *)arguments)->generating;*/
-
+void *alienGen(void *arguments){
     struct argsGen * args = (struct argsGen *) arguments;
 
     printf("Empieza Generacion\n");
@@ -105,23 +119,23 @@ void *printAliens(void *arguments){
 
 }
 
-void calen_prioridad(void *arguments){
+void *calen_prioridad(void *arguments){
     printf("Empieza el calndarizador\n");
     struct argsCalenPrior * args = (struct argsCalenPrior *)arguments;
     int count = 0;
     int size = 0;
     while(1){
-        printf("Generating: %d\n",*args->generating);
-        printf("Ready: %d\n", *args->ready);
+        //printf("Generating: %d\n",*args->generating);
+        //printf("Ready: %d\n", *args->ready);
         pthread_mutex_lock(args->lockColonia);
         size = getSize(args->colonia);
         if(*args->generating == 0 && size == 0){
             pthread_mutex_unlock(args->lockColonia);
             break;
         }
-        printf("size: %d\n", size);
+        //printf("size: %d\n", size);
         if(*args->ready == 1 && getSize(args->extremo) < args->cantExtremos){
-            printf("Entra al if\n");
+            //printf("Entra al if\n");
             int index = 0;
             struct Alien *prior = (struct Alien *) malloc(sizeof(struct Alien));
             prior->prioridad = -1;
@@ -135,21 +149,81 @@ void calen_prioridad(void *arguments){
                     index = i;
                 }
             }
-            addLast(args->extremo, prior);
-            printf("Hace Delete index = %d el size es %d\n", index, size);
-            delById(args->colonia, index);
-            pthread_mutex_unlock(args->lockColonia);
-            count++;
+            if(prior->prioridad != -1){
+                addLast(args->extremo, prior);
+                //printf("Hace Delete index = %d el size es %d\n", index, size);
+                delById(args->colonia, index);
+                pthread_mutex_unlock(args->lockColonia);
+                count++;
+            }
         }else{
             pthread_mutex_unlock(args->lockColonia);
-            printf("Sali del unlock\n");
+            //printf("Sali del unlock\n");
         }
-        sleep(1);
     }
 }
 
-int main(){
-    /*
+void *alienThr(void *arguments){
+    
+}
+
+void *bridgeY(void *arguments){
+    struct argsBridgeY *args = (struct argsBridgeY *) arguments;
+    llist *aliensEnPuente = create(NULL);
+    int sentido = 0;
+    int pesoEnmomento = 0;
+    int aliensTotal = 0;
+    int aliensPasados = 0;
+    llist *extremoTrab;
+    pthread_mutex_t *lockExtremo;
+    pthread_mutex_t lockAliensPuente;
+
+    pthread_mutex_init(&lockAliensPuente, NULL); 
+    while(1){
+        printf("sentido: %d, pesoEnmomento: %d, AliensTotal: %d, AliensPasados: %d\n", sentido, pesoEnmomento, aliensTotal, aliensPasados);
+        if(aliensTotal > args->Y && aliensTotal == 0){
+            aliensTotal = 0;
+            if(sentido == 0){
+                sentido = 1;
+            }else{
+                sentido = 0;
+            }
+        }
+        if(sentido == 0){
+            extremoTrab = args->extremoN;
+            lockExtremo = args->lockExtremoN;
+        }else{
+            extremoTrab = args->extremoS;
+            lockExtremo = args->lockExtremoS;
+        }
+        pthread_mutex_lock(lockExtremo);
+        if(getSize(extremoTrab) > 0){
+            struct Alien *alienAct = (struct Alien *) getbyId(extremoTrab, 0);
+            if((pesoEnmomento + alienAct->peso) <= args->pesoSoport){
+                struct alienCruzando *alientTemp = (struct alienCruzando *) malloc(sizeof(struct alienCruzando));
+                pthread_t tid;
+                pesoEnmomento = pesoEnmomento + alienAct->peso;
+                alientTemp->alien = alienAct;
+                alientTemp->indice = aliensPasados;
+                alientTemp->pos = 0;
+                printf("Aqui deberia generar un thread \n");
+                numprint(alientTemp->alien);
+                //pthread_create(&alientTemp->tid, NULL, alienThr, (void *)alientTemp);
+                aliensPasados++;
+                aliensTotal++;
+                delById(extremoTrab, 0);
+            }
+        }
+        pthread_mutex_unlock(lockExtremo);
+        if(args->generatingN == 0  && args->generatingS == 0 && getSize(args->extremoN) == 0 && getSize(args->extremoS) == 0){
+            break;
+        }
+        sleep(1);
+    }
+    pthread_mutexattr_destroy(&lockAliensPuente);
+}
+
+int main(){/*
     llist *colonia1 = create(NULL);
 
     struct Alien * alien = (struct Alien *) malloc(sizeof(struct Alien));
@@ -170,9 +244,15 @@ int main(){
 
     addLast(colonia1,alien2);
 
+    struct Alien* alienGet = (struct Alien *) getbyId(colonia1, 0);
+
     delById(colonia1, 0);
 
-    print(colonia1, numprint); */   
+    printf("Colonia :\n");
+    print(colonia1, numprint);
+
+    printf("Alien suelto: \n");
+    numprint(alienGet);*/
 
 
     pthread_mutex_t lockColonia1, lockColonia2, lockExtremoN, lockExtremoS;
@@ -186,7 +266,6 @@ int main(){
 
     srand((unsigned)time(NULL));
     double sleep_time = ran_expo(0.5);
-
     llist *colonia1 = create(NULL);
 
     llist *colonia2 = create(NULL);
@@ -202,7 +281,7 @@ int main(){
 
     Params->interval = sleep_time;
     Params->mylist = colonia1;
-    Params->total = 5;
+    Params->total = 6;
     Params->velocidad = 5;
     Params->lockColonia = &lockColonia1;
     Params->generating = 1;
@@ -221,7 +300,7 @@ int main(){
 
     Params2->interval = sleep_time;
     Params2->mylist = colonia2;
-    Params2->total = 5;
+    Params2->total = 6;
     Params2->velocidad = 5;
     Params2->lockColonia = &lockColonia2;
     Params2->generating = 1;
@@ -253,8 +332,21 @@ int main(){
     ParamsCalenN2->ready = &Params2->ready;
     ParamsCalenN2->cantExtremos = 5;
 
+    struct argsBridgeY *bridge1 = (struct argsBridgeY *) malloc(sizeof(struct argsBridgeY));
 
-    pthread_t tid1, tid2, tid3, tid4;
+
+    bridge1->extremoN = extremoN;
+    bridge1->extremoS = extremoS;
+    bridge1->generatingN = Params->generating;
+    bridge1->generatingS = Params2->generating;
+    bridge1->lenBr = 100;
+    bridge1->lockExtremoN = &lockExtremoN;
+    bridge1->lockExtremoS = &lockExtremoS;
+    bridge1->pesoSoport = 15;
+    bridge1->Y = 3;
+
+
+    pthread_t tid1, tid2, tid3, tid4, tid5;
 
     pthread_create(&tid1, NULL, alienGen, (void *)Params);
     
@@ -264,10 +356,11 @@ int main(){
 
     pthread_create(&tid4, NULL, calen_prioridad, (void *)ParamsCalenN2);
 
+    pthread_create(&tid5, NULL, bridgeY, (void *)bridge1);
 
 
     //pthread_create(&tid3, NULL, printAliens, (void *)ParamsPrint);
-    //pthread_create(&tid4, NULL, printAliens, (void *)ParamsPrint2);*/
+    //pthread_create(&tid4, NULL, printAliens, (void *)ParamsPrint2);
 
     pthread_join(tid1, NULL);
 
@@ -298,13 +391,15 @@ int main(){
     printf("Aliens calend en extremo2\n");
     print(extremoS, numprint);
 
+    pthread_join(tid5, NULL);
+
     //lfree(colonia1);
-    //lfree(colonia2);*/
+    //lfree(colonia2);
     pthread_mutex_destroy(&lockColonia1); 
     pthread_mutex_destroy(&lockColonia2);
 
     pthread_mutex_destroy(&lockExtremoN); 
-    pthread_mutex_destroy(&lockExtremoS); 
+    pthread_mutex_destroy(&lockExtremoS);
 
     return 0;
 }
